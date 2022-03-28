b2e0584879c8d454ff4a809807efc0057134d99a
CintiaGdl <cintiamgori@gmail.com>
2022-03-28 14:10:47 +0200

    all buttons

--- src/App.js
+++ src/App.js
@@ -8,32 +8,58 @@ import PersonalData from './components/PersonalData/PersonalData';
 import Education from './components/Education_Experience/Education';
 import Experience from './components/Education_Experience/Experience';
 import Habilities from './components/More/Habilities';
-import About from './components/About/About';
+// import About from './components/About/About';
 import Languages from './components/More/Languages';
 
 const { personalData, education, experience, languages, habilities } = CV;
 
 function App() {
+  
+  const [showSection, setshowSection] = useState("edu");
 
-  const [showEducation, setShowEducation] = useState(true);
+  const change = (() => {
+      if (showSection==="edu") {
+          return <Education education={education} />
+      }
+      if (showSection==="exp") {
+        return <Experience experience={experience} />
+      }
+      if (showSection==="more") {
+        return (
+          <div className='sectionCard2'>
+          <div className='languages'>
+            <h2>Languages: </h2>
+            <Languages languages={languages} />
+          </div>
+          <div className='languages'>
+            <h2>Habilities: </h2>
+            <Habilities habilities={habilities} />
+          </div>
+        </div>
+        )
+      }
+  })
 
   return (
     <div className="App">
       <PersonalData data={personalData} />
-      <About about={personalData.aboutMe} />
+      {/* <About about={personalData.aboutMe} /> */}
       {/* <Education education={education} />
         <Experience experience={experience} /> */}
-      <button className='buttonChange' onClick={() => setShowEducation(true)}>Education</button>
-      <button className='buttonChange' onClick={() => setShowEducation(false)}>Experience</button>
-      {/* <button className='buttonChange' onClick={() => setShowEducation(more)}>More</button> */}
+      <div className='cardButton'>
+        <button className='buttonChange' onClick={() => setshowSection("edu")}>Education</button>
+        <button className='buttonChange' onClick={() => setshowSection("exp")}>Experience</button>
+        <button className='buttonChange' onClick={() => setshowSection("more")}>More</button>
+      </div>
       <div>
-        {showEducation ? (
+       {/*  {(showEducation==="edu") ? (
           <Education education={education} />
         ) : (
           <Experience experience={experience} />
-        )}
+        )} */}
+        {change(showSection)}
       </div>
-      <div className='sectionCard2'>
+      {/* <div className='sectionCard2'>
         <div className='languages'>
           <h2>Languages: </h2>
           <Languages languages={languages} />
@@ -42,7 +68,7 @@ function App() {
           <h2>Habilities: </h2>
           <Habilities habilities={habilities} />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ src/components/Button/ChangeSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Education from '../Education_Experience/Education';
+import { useState } from 'react';
+const { education } = CV;
+
+export default function ChangeSection(section) {
+    const [showSection, setshowSection] = useState(educationSection);
+    setshowSection(section)
+  return (
+      
+}
+
